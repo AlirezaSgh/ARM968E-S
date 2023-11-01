0662ec27a73c0884478bcb9561bbc05b3cfcd601
alirezasgh <s.a.saghaeian@gmail.com>
2023-11-01 09:25:35 +0330

    update ID_Stage

--- Codes/Control_Unit.v
+++
@@ -1,3 +0,0 @@
-module Control_Unit(input [1:0] mode, input [3:0] OPCode, input S);
-
-endmodule
--- Codes/ID_Stage.v
+++ Codes/ID_Stage.v
@@ -23,20 +23,23 @@ module ID_Stage(
     output Two_src
 );
     wire [11:0] OPC_shifter_operand;
-    wire [3:0] cond, OPCOde, Rn, Rd, rotate_imm, immed_8, CU_EXE_CMD;
+    wire [3:0] cond, OPCode, Rn, Rd, rotate_imm, immed_8, CU_EXE_CMD;
     wire [1:0] OPC_mode;
-    wire OPC_I, OPC_S, CU_B, CU_S, CU_WB_EN, CU_MEM_R_EN, CU_MEM_W_EN;
+    wire OPC_S, CU_B, CU_S, CU_WB_EN, CU_MEM_R_EN, CU_MEM_W_EN, cond_flag, zero_sel, not_cond_flag;
     assign cond = Instruction[31:28];
     assign mode = Instruction[27:26];
-    assign OPC_I = Instruction[25];
+    assign imm = Instruction[25];
     assign OPCode = Instruction[24:21];
     assign OPC_S = Instruction[20];
     assign Rn = Instruction[19:16];
     assign Rd = Instruction[15:12];
     assign OPC_shifter_operand = Instruction[11:0];
+    assign not_cond_flag = ~cond_flag;
+    assign zero_sel = not_cond_flag | hazard;
+    assign Two_src = (~imm) | MEM_W_EN;
     MUX register_file_mux(Rd, Instruction[3:0], MEM_W_EN);
-    MUX #9 cond_mux({CU_EXE_CMD, CU_WB_EN, CU_MEM_R_EN, CU_MEM_W_EN, CU_B, CU_S}, 9'b0,,{EXE_CMD, WB_EN, MEM_R_EN, MEM_W_EN, B, S});
-    Condition_Check condition_check();
+    MUX #9 cond_mux({CU_EXE_CMD, CU_WB_EN, CU_MEM_R_EN, CU_MEM_W_EN, CU_B, CU_S}, 9'b0, zero_sel,{EXE_CMD, WB_EN, MEM_R_EN, MEM_W_EN, B, S});
+    Condition_Check condition_check(cond, OPCode,cond_flag);
     RegisterFile register_file(clk, rst, Rn, , Des_wb, Result_WB, writeBackEN, Val_Rn, Val_Rm);
-    ControlUnit control_unit(OPCode, mode, OPC_S, OPC_I, CU_EXE_CMD, CU_WB_EN, CU_MEM_R_EN, CU_MEM_W_EN, CU_B, CU_S);
+    ControlUnit control_unit(OPCode, mode, OPC_S, imm, CU_EXE_CMD, CU_WB_EN, CU_MEM_R_EN, CU_MEM_W_EN, CU_B, CU_S);
 endmodule
